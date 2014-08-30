998c630bae13b617952e066d6fe9d7545685cb9e
Abhayagiri Publications <abmpubs@abhayagiri.org>
2014-08-30 15:20:05 -0700

    Added script for arranging talk headers in markdown

---
+++ scripts/mdheads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Usage ./md-heads < in.md > out.md
+# Takes vanilla markdown from LibreOffice html | `pandoc -t markdown` and
+# arranges the title/author/date sequence while adding ATX-sytle headers
+import sys
+
+authors = (
+    "Luang Por Pasanno\n",
+    "Ajahn Amaro\n",
+    "Ajahn Yatiko\n",
+    "Ajahn Karuṇadhammo\n",
+    "Ajahn Joitpālo\n",
+    "Ajahn Ñāṇiko\n",
+    "Ajahn Vīradhammo\n",
+    "Ajahn Sucitto\n",
+)
+
+infile = sys.stdin
+outfile = sys.stdout
+
+line = infile.readline()
+
+while line:
+    if line in authors:
+        author = line
+        infile.readline() # blank
+        date = infile.readline()
+        infile.readline() # blank
+        title = infile.readline()
+        outfile.write("# " + title + "\n")
+        outfile.write("## " + author + "\n")
+        outfile.write("### " + date)
+    else:
+        outfile.write(line)
+    line = infile.readline()
