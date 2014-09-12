b47d796d353038d53271298d5ae9894058d746d0
Abhayagiri Publications <abmpubs@abhayagiri.org>
2014-09-12 13:33:44 -0700

    utils.py is no longer needed

--- scripts/utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import re
-
-def mangle_title(title):
-    title = title.lower()
-    title = re.sub(ur"[^a-zāīūṃṅñṭḍṇḷ\s]", "", title)
-    title = re.sub(r"\s+", "-", title)
-    return title
