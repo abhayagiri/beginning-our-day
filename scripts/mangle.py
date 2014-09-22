bc6289f4de97670298e5b6d74b18e02f6542a571
Abhayagiri Publications <abmpubs@abhayagiri.org>
2014-09-22 16:17:09 -0700

    Bugfix in mangle.py to handle question marks correctly

--- scripts/mangle.py
+++ scripts/mangle.py
@@ -5,6 +5,6 @@ import re
 def mangle(u):
     u = u.strip ('\n')
     u = u.lower()
-    u = re.sub(r"[:']", '', u)
+    u = re.sub(r"[:'&,?]", '', u)
     u = re.sub(ur'[^a-zāūīṃṅñṭḍṇḷ]', '_', u)
     return u
