75696b891cbc722cb9f3c1848ae0fbeb17d012b6
Abhayagiri Publications <abmpubs@abhayagiri.org>
2014-09-21 17:25:33 -0700

    Added mangle.py for mangling titles

---
+++ scripts/mangle.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+import re
+
+
+def mangle(u):
+    u = u.strip ('\n')
+    u = u.lower()
+    u = re.sub(r"[:']", '', u)
+    u = re.sub(ur'[^a-zāūīṃṅñṭḍṇḷ]', '_', u)
+    return u
