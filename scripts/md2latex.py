fc534c5552dd69cc406b594ea1c2c21b6fa5e60b
Abhayagiri Publications <abmpubs@abhayagiri.org>
2014-11-26 09:48:39 -0800

    Cleaned up md2latex.py

--- scripts/md2latex.py
+++ scripts/md2latex.py
@@ -2,40 +2,13 @@
 # -*- encoding: utf-8 -*-
 # Usage: html2latex.py < INFILE.html > OUTFILE.tex
 import sys
-from cStringIO import StringIO
 import os
 import re
 import subprocess
 from lxml import etree
 from lxml import sax
 import xml.sax.handler
-
-authors = {
-    u"Luang Por Pasanno": "lpp",
-    u"Ajahn Amaro": "aa",
-    u"Ajahn Yatiko": "ay",
-    u"Ajahn Karuṇadhammo": "akd",
-    u"Ajahn Jotipālo": "ajo",
-    u"Ajahn Ñāṇiko": "an",
-    u"Ajahn Vīradhammo": "av",
-    u"Ajahn Sucitto": "asuc",
-}
-
-months = {
-    "01": "January",
-    "02": "February",
-    "03": "March",
-    "04": "April",
-    "05": "May",
-    "06": "June",
-    "07": "July",
-    "08": "August",
-    "09": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December",
-}
-
+from encodings import utf_8
 
 class Html2Latex(xml.sax.handler.ContentHandler):
     def __init__(self, outfile=sys.stdout):
@@ -58,20 +31,20 @@ class Html2Latex(xml.sax.handler.ContentHandler):
             content = re.sub(ur'\u2014', '---', content)
             cur = self.path[-1]
             if cur == 'h1':
-                self.curtitle = content.encode('utf-8')
+                self.curtitle = content
             elif cur == 'h2':
                 m = re.match(ur'(.*) • (.*)', content)
                 if m is None:
                     print "WHOOPS"
-                    print content.encode('utf-8')
+                    print content
                     sys.exit(1)
-                self.curauthor = m.group(1).encode('utf-8')
-                self.curdate = m.group(2).encode('utf-8')
+                self.curauthor = m.group(1)
+                self.curdate = m.group(2)
             #elif cur == 'h3':
             #    m = re.match(r'(..)-(..)-(..)', content)
             #    self.curdate = '%s 20%s' % (months[m.group(1)], m.group(3))
             else:
-                self.outfile.write(content.encode('utf-8'))
+                self.outfile.write(content)
     def startElementNS(self, name, qname, attrs):
         self.path.append(qname)
         if qname == 'p':
@@ -100,17 +73,17 @@ infile = sys.stdin
 outfile = sys.stdout
 
 
-
 pandoc = subprocess.Popen(['pandoc', '-f', 'markdown', '-t', 'html'],
     stdin=infile, stdout=subprocess.PIPE)
 sed = subprocess.Popen(['sed', 's/^[[:space:]]*//'],
     stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+coder = utf_8.StreamWriter(sed.stdin)
 fold = subprocess.Popen(['fold', '-s', '-w', '72'],
     stdin=sed.stdout, stdout=outfile)
 #pandoc.stdin.write(infile.read())
 #pandoc.stdin.close()
 tree = etree.parse(pandoc.stdout, etree.HTMLParser(encoding='utf-8'))
-sax.saxify(tree, Html2Latex(sed.stdin))
+sax.saxify(tree, Html2Latex(coder))
 sed.stdin.close()
 
 #outfile.write(fold.stdout.read())
